85fc7109a283f6656c227092bc7e3068722a5f5e
Deepak Singh <deepkushwah824@gmail.com>
2023-08-30 19:18:29 +0530

    #89

--- app/src/main/java/com/example/pwdschool/Login.java
+++ app/src/main/java/com/example/pwdschool/Login.java
@@ -2,12 +2,9 @@ package com.example.pwdschool;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
