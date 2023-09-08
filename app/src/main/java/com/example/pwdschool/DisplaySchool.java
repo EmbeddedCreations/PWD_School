fb27e67526050e5b2f62c6d6f3dcc51545fd70d9
Deepak Singh <deepkushwah824@gmail.com>
2023-09-08 23:59:06 +0530

    #87

--- app/src/main/java/com/example/pwdschool/DisplaySchool.java
+++ app/src/main/java/com/example/pwdschool/DisplaySchool.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,12 +21,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DisplaySchool extends Fragment {
-
 
     public static String selectedSchoolHistory;
     String address = "https://www.embeddedcreation.in/tribalpwd/adminPanelNewVer2/appFetchSchools.php?user=" + Home.juniorEngineer;
@@ -52,15 +51,17 @@ public class DisplaySchool extends Fragment {
 
         List<SchoolClass> Schools = new ArrayList<>();
         for (Map.Entry<String, String> entry : schoolBuildingsMap.entrySet()) {
-
             Schools.add(new SchoolClass(entry.getKey(), entry.getValue()));
         }
+
+        // Reverse the order of the Schools list
+        Collections.reverse(Schools);
+
         recyclerView = getView().findViewById(R.id.schoolRecyclerView);
         schoolClassAdapter = new SchoolClassAdapter(Schools);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(schoolClassAdapter);
-
     }
 
     public void getData() {
@@ -89,7 +90,7 @@ public class DisplaySchool extends Fragment {
                 String building = jo.getString("image_name");
                 if (schoolBuildingsMap.containsKey(school)) {
                     String existingBuildings = schoolBuildingsMap.get(school);
-                    schoolBuildingsMap.put(school, existingBuildings + ", " + building);
+                    schoolBuildingsMap.put(school, building + ", " + existingBuildings);
                 } else {
                     schoolBuildingsMap.put(school, building);
                 }
@@ -97,6 +98,5 @@ public class DisplaySchool extends Fragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
