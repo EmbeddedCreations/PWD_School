5c2e4d33793aff3d829ce2c4d402bb3ef26bd007
Deepak Singh <deepkushwah824@gmail.com>
2023-09-10 17:46:00 +0530

    #100

--- app/src/main/java/com/example/pwdschool/WorkOrderCheckSheet.java
+++ app/src/main/java/com/example/pwdschool/WorkOrderCheckSheet.java
@@ -19,8 +19,6 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import java.util.Arrays;
-
 public class WorkOrderCheckSheet extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
@@ -33,16 +31,28 @@ public class WorkOrderCheckSheet extends Fragment implements AdapterView.OnItemS
         int numRows = rowHeadings.length;
         int numCols = columnHeadings.length;
 
-// Specify which checkboxes to precheck for odd rows (e.g., columns 2 and 4)
-        int[] precheckedColumns = {1,2, 4};
+        // Example checkboxStates array
+        int[][] checkboxStates = {
+                {1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
 
-        createDynamicTable(view, numRows, numCols, columnHeadings, rowHeadings, precheckedColumns);
-
+        createDynamicTable(view, numRows, numCols, columnHeadings, rowHeadings, checkboxStates);
 
         return view;
     }
 
-    private void createDynamicTable(View view, int numRows, int numCols, String[] colLabels, String[] rowLabels, int[] precheckedColumns) {
+    private void createDynamicTable(View view, int numRows, int numCols, String[] colLabels, String[] rowLabels, int[][] checkboxStates) {
         TableLayout tableLayout = view.findViewById(R.id.tableLayout);
 
         // Create header row with column labels and checkboxes
@@ -62,49 +72,69 @@ public class WorkOrderCheckSheet extends Fragment implements AdapterView.OnItemS
         tableLayout.addView(headerRow);
 
         // Create rows with labels and checkboxes
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 1; i <= numRows; i++) {
             TableRow row = new TableRow(requireContext());
 
             for (int j = 0; j <= numCols; j++) {
                 if (j == 0) {
                     // Add row label (without checkbox)
                     TextView textView = new TextView(requireContext());
-                    textView.setText(rowLabels[i]);
+                    textView.setText(rowLabels[i - 1]);
                     row.addView(textView);
                 } else {
                     // Add four checkboxes in the data cells (including the new row)
                     for (int k = 0; k < 4; k++) {
+                        final int checkboxIndex = (j - 1) * 4 + k; // Make 'j' effectively final
+                        final int columnIndex = j; // Make 'j' effectively final
+                        final int checkboxColumnIndex = k; // Make 'k' effectively final
+
                         CheckBox checkBox = new CheckBox(requireContext());
 
-                        // Check if the current row index is odd and if the current checkbox column is in precheckedColumns
-                        if (i % 2 == 0 && Arrays.binarySearch(precheckedColumns, j) >= 0) {
-                            checkBox.setChecked(true);
-                            checkBox.setEnabled(false);
-                        } else {
-                            // Add an OnTouchListener to block unchecking
-                            checkBox.setOnTouchListener(new View.OnTouchListener() {
-                                @Override
-                                public boolean onTouch(View v, MotionEvent event) {
-                                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                        CheckBox clickedCheckBox = (CheckBox) v;
-                                        // If the checkbox is checked, display a toast and block unchecking
-                                        if (clickedCheckBox.isChecked()) {
-                                            Toast.makeText(requireContext(), "Checkbox is now checked and cannot be unchecked.", Toast.LENGTH_SHORT).show();
-                                            return true; // Block touch events
-                                        }
-                                    }
-                                    return false; // Allow touch events
+                        // Check if the checkbox should be checked based on the checkboxStates array
+                        checkBox.setChecked(checkboxStates[i - 1][checkboxIndex] == 1);
+
+                        // Add an OnCheckedChangeListener to prevent unchecking and update the array
+                        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if (!isChecked) {
+                                    // If the checkbox is unchecked, force it to be checked
+                                    checkBox.setChecked(true);
+                                    return;
                                 }
-                            });
+                                // Update the corresponding value in the checkboxStates array
+                                int rowIndex = tableLayout.indexOfChild(row);
+                                int checkboxArrayIndex = (columnIndex - 1) * 4 + checkboxColumnIndex;
+                                checkboxStates[rowIndex - 1][checkboxArrayIndex] = isChecked ? 1 : 0;
+
+                                // Print the updated array to the console
+                                for (int i = 0; i < numRows; i++) {
+                                    for (int j = 0; j < numCols * 4; j++) {
+                                        System.out.print(checkboxStates[i][j] + " ");
+                                    }
+                                    System.out.println(); // Move to the next row
+                                }
+                            }
+                        });
+
+
+                        // Set checkboxes in even rows to be disabled
+                        if ((i - 1) % 2 == 0) {
+                            checkBox.setEnabled(false);
                         }
+
                         row.addView(checkBox);
                     }
                 }
             }
 
+
+
+
             tableLayout.addView(row);
         }
     }
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -116,6 +146,7 @@ public class WorkOrderCheckSheet extends Fragment implements AdapterView.OnItemS
             getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
     }
+
     private void showLandscapeModeDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle("Landscape Mode");
@@ -131,7 +162,6 @@ public class WorkOrderCheckSheet extends Fragment implements AdapterView.OnItemS
         AlertDialog dialog = builder.create();
         dialog.show();
     }
-
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
